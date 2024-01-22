b6c2bedc763e48192f77c014e38c01dcff5763bf
paolosait <paolosait7@gmail.com>
2024-01-22 17:46:32 +0000

    Connected to sanity project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '5nxp86fq',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/test.ts
@@ -0,0 +1,13 @@
+// schemas/pet.js
+export default {
+    name: 'test',
+    type: 'document',
+    title: 'Test',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        }
+    ]
+}
