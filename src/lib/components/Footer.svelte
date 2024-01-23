cb3605e0572373946ab6662276d5ce2e38e5c0c4
paolosait <paolosait7@gmail.com>
2024-01-23 16:02:59 +0000

    Added Flowbite and basic layout

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,67 @@
+<script>
+    import {
+        Footer,
+        FooterLinkGroup,
+        FooterLink,
+        FooterCopyright,
+    } from "flowbite-svelte";
+</script>
+
+<Footer footerType="sitemap">
+    <div class="grid grid-cols-2 gap-8 py-8 px-6 md:grid-cols-4">
+        <div>
+            <h2 class="mb-6 text-sm font-semibold text-gray-400 uppercase">
+                Company
+            </h2>
+            <FooterLinkGroup ulClass="text-gray-300">
+                <FooterLink liClass="mb-4" href="/">About</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Careers</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Brand Center</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Blog</FooterLink>
+            </FooterLinkGroup>
+        </div>
+        <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">
+                Download
+            </h2>
+            <FooterLinkGroup ulClass="text-gray-300">
+                <FooterLink liClass="mb-4" href="/">Discord Server</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Twitter</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Facebook</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Contact Us</FooterLink>
+            </FooterLinkGroup>
+        </div>
+        <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">
+                Legal
+            </h2>
+            <FooterLinkGroup ulClass="text-gray-300">
+                <FooterLink liClass="mb-4" href="/">Privacy Policy</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Licensing</FooterLink>
+                <FooterLink liClass="mb-4" href="/"
+                    >Terms & Conditions</FooterLink
+                >
+            </FooterLinkGroup>
+        </div>
+        <div>
+            <h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">
+                Download
+            </h2>
+            <FooterLinkGroup ulClass="text-gray-300">
+                <FooterLink liClass="mb-4" href="/">iOS</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Android</FooterLink>
+                <FooterLink liClass="mb-4" href="/">Windows</FooterLink>
+                <FooterLink liClass="mb-4" href="/">MacOS</FooterLink>
+            </FooterLinkGroup>
+        </div>
+    </div>
+    <div
+        class="py-6 px-4 bg-gray-700 md:flex md:items-center md:justify-between"
+    >
+        <FooterCopyright
+            spanClass="text-sm text-gray-300 sm:text-center"
+            href="/"
+            by="TBC™"
+        />
+    </div>
+</Footer>
---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,84 @@
+<script>
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+        Dropdown,
+        DropdownItem,
+        MegaMenu,
+    } from "flowbite-svelte";
+    import { ChevronDownSolid } from "flowbite-svelte-icons";
+   
+    let menu = [
+        { name: "Free CBT Sessions", href: "/" },
+        { name: "Free VR Psychosexual Therapy", href: "/" },
+        { name: "Facebook Support Group", href: "/" },
+        { name: "Our Online Forum", href: "/" },
+        { name: "Patient Stories", href: "/" },
+    ];
+</script>
+
+<Navbar>
+    <NavBrand href="/">
+        <img
+            src="/temporary_logo.png"
+            class="me-3 h-6 sm:h-9"
+            alt="temporary logo"
+        />
+        <span
+            class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white"
+            >Cauda Equina Therapy</span
+        >
+    </NavBrand>
+    <NavHamburger />
+    <NavUl>
+        <NavLi class="cursor-pointer">
+            Cauda Equina Syndrome<ChevronDownSolid
+                class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline"
+            />
+        </NavLi>
+        <Dropdown offset="18">
+            <DropdownItem>What is Cauda Equina?</DropdownItem>
+            <DropdownItem>Cauda Equina Syndrome</DropdownItem>
+            <DropdownItem>Diagnosis</DropdownItem>
+            <DropdownItem>Rehabilitation</DropdownItem>
+            <DropdownItem>Treatment</DropdownItem>
+        </Dropdown>
+
+        <NavLi class="cursor-pointer">
+            Bladder & Bowel Dysfunction<ChevronDownSolid
+                class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline"
+            />
+        </NavLi>
+        <Dropdown offset="18">
+            <DropdownItem>Urodynamics</DropdownItem>
+            <DropdownItem>Pelvic Floor Exercises</DropdownItem>
+            <DropdownItem>Spinal Cord Stimulators</DropdownItem>
+        </Dropdown>
+
+        <NavLi class="cursor-pointer">
+            Support<ChevronDownSolid
+                class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline"
+            />
+        </NavLi>
+        <MegaMenu items={menu} let:item>
+            <a
+                href={item.href}
+                class="flex items-center hover:text-primary-600 dark:hover:text-primary-500"
+                >{item.name}
+            </a>
+        </MegaMenu>
+
+        <NavLi class="cursor-pointer">
+            Research<ChevronDownSolid
+                class="w-3 h-3 ms-2 text-primary-800 dark:text-white inline"
+            />
+        </NavLi>
+        <Dropdown offset="18">
+            <DropdownItem>Surgical Interventions</DropdownItem>
+            <DropdownItem>Stem Cell Biology</DropdownItem>
+        </Dropdown>
+    </NavUl>
+</Navbar>
