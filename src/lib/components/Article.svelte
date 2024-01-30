02647bd76862e94417d94fe93e117df914e16ebc
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2024-01-30 20:25:45 +0000

    TK: articles section

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,6 +1,7 @@
-<div class="bg-white p-4 flex flex-col h-60 justify-between">
-    <div class="bg-[#F4F4F4] h-20"></div>
-    <div class="font-bold"> X organisation does ABC research to show results about ?.</div>
+<div class="w-full bg-white p-4 flex flex-col h-80">
+    <div class="bg-[#F4F4F4] h-1/2"></div>
+    <div class="font-bold mt-2"> X organisation does ABC research to show results about ?.</div>
+    <div class="grow"></div>
     <div class="flex">
         <div>18th Jan 2024</div>
         <div class="grow"></div>
--- src/lib/components/ArticlesSection.svelte
+++ src/lib/components/ArticlesSection.svelte
@@ -2,10 +2,12 @@
     import Article from "./Article.svelte";
 
 </script>
-<div class="bg-[#F4F4F4] h-80 px-10 py-5">
-    <div class="border-2 border-t-0 border-b-0 border-l-[#60C4F0] border-r-[#60C4F0] h-full flex flex-col px-5">
-        <div class="font-bold"> The latest news about spinal injuries</div>
-        <div class="w-full flex flex-row">
+<div class="bg-[#F4F4F4] px-10 py-5">
+    <div class="border-2 border-t-0 border-b-0 border-l-[#60C4F0] border-r-[#60C4F0] h-full flex flex-col px-10 gap-8">
+        <div class="text-lg font-bold"> The latest news about spinal injuries:</div>
+        <div class="w-full flex flex-row gap-32">
+            <Article />
+            <Article />
             <Article />
         </div>
     </div>
