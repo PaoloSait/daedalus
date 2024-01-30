482052abaa4e96d881f2d8db7f7a76a89ccad4b9
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2024-01-30 23:24:16 +0000

    TK Added info section

---
+++ src/lib/components/InfoSection.svelte
@@ -0,0 +1,20 @@
+<div class="flex flex-col items-center px-20 gap-5 my-10">
+    <div class="text-xl font-bold">What is Cauda Equina?</div>
+    <div class="text-lg">
+        Cauda Equina Syndrome (CES) is a rare but serious medical condition that
+        occurs when the nerve roots of the cauda equina, located at the base of
+        the spinal cord, become compressed. This compression can result from
+        various causes such as a herniated disc, spinal tumors, trauma, or
+        spinal infections. Individuals with CES may experience a range of
+        symptoms, including severe lower back pain, leg weakness, numbness in
+        the lower extremities, and loss of bowel or bladder control. Recognizing
+        these symptoms and seeking immediate medical attention is crucial, as
+        CES requires prompt intervention to prevent irreversible damage to the
+        nerves.
+    </div>
+    <div class="bg-[#CAEFFF] w-full flex flex-col rounded-lg px-2 py-2 gap-2">
+        <div class="text-xl font-bold">Did you know?</div>
+        <div class="text-lg"> The average medical malpractice payout for children under one month old is almost $1 Million. Let us help you get the payout you deserve</div>
+        <div class="bg-black text-white w-1/5 pl-3 py-3 rounded-lg"> Get free case review -></div>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     import Tagline from "$lib/components/Tagline.svelte";
     import ArticlesSection from "$lib/components/ArticlesSection.svelte"
+    import InfoSection from "$lib/components/InfoSection.svelte";
     export let data;
 </script>
 
 <div>
     <Tagline />
     <ArticlesSection />
+    <InfoSection />
     <div class="flex-col flex gap-4 px-10">
     {#each data.tests as test}
         <p>{test.name}</p>
