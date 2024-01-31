89a66b985567fa98efbf2195545e5259767856a8
paolosait <paolosait7@gmail.com>
2024-01-31 18:23:31 +0000

    minor visual tweaks

--- src/lib/components/InfoSection.svelte
+++ src/lib/components/InfoSection.svelte
@@ -1,5 +1,5 @@
-<div class="flex flex-col items-center px-20 gap-5 my-10">
-    <div class="text-xl font-bold">What is Cauda Equina?</div>
+<div class="flex flex-col items-center px-40 gap-5 my-20">
+    <div class="text-3xl font-bold">What is Cauda Equina?</div>
     <div class="text-lg">
         Cauda Equina Syndrome (CES) is a rare but serious medical condition that
         occurs when the nerve roots of the cauda equina, located at the base of
@@ -12,9 +12,9 @@
         CES requires prompt intervention to prevent irreversible damage to the
         nerves.
     </div>
-    <div class="bg-[#CAEFFF] w-full flex flex-col rounded-lg px-2 py-2 gap-2">
-        <div class="text-xl font-bold">Did you know?</div>
+    <div class="bg-[#CAEFFF] w-full flex flex-col rounded-xl px-4 py-4 gap-3">
+        <div class="text-2xl font-bold">Did you know?</div>
         <div class="text-lg"> The average medical malpractice payout for children under one month old is almost $1 Million. Let us help you get the payout you deserve</div>
-        <div class="bg-black text-white w-1/5 pl-3 py-3 rounded-lg"> Get free case review -></div>
+        <div class="bg-black text-white font-medium pl-3 py-3 rounded-lg w-min min-w-56"><div>Get free case review -></div></div>
     </div>
 </div>
