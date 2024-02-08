c155904e6ecfba4344b353f5e027ca31b768582b
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2024-02-08 18:07:46 +0000

    TK: Updated get case review block

--- src/lib/components/InfoSection.svelte
+++ src/lib/components/InfoSection.svelte
@@ -12,17 +12,27 @@
         CES requires prompt intervention to prevent irreversible damage to the
         nerves.
     </div>
-    <div class="bg-[#CAEFFF] w-full flex flex-col rounded-xl px-4 py-4 gap-3 z-10">
+    <div
+        class="bg-[#CAEFFF] w-full flex flex-col rounded-xl px-4 py-4 gap-3 z-10 shadow-md"
+    >
         <div class="text-2xl font-bold">Did you know?</div>
         <div class="text-lg">
             The average medical malpractice payout for children under one month
             old is almost $1 Million. Let us help you get the payout you deserve
         </div>
-        <div
-            class="bg-black text-white font-medium pl-3 py-3 rounded-lg w-min min-w-56"
-        >
-            <div>Get free case review -></div>
-        </div>
+        <a href="/contact">
+            <div
+                class="bg-black text-white font-medium px-3 py-3 rounded-lg w-min min-w-52"
+            >
+                <div class="flex flex-row gap-2">
+                    Get free case review <span
+                        class="material-symbols-outlined"
+                    >
+                        arrow_forward
+                    </span>
+                </div>
+            </div>
+        </a>
     </div>
     <div class="custom-shape-divider-bottom-1706781815">
         <svg
@@ -38,9 +48,7 @@
         </svg>
     </div>
 </div>
-<div class="w-full h-20 bg-[#f4f4f4]">
-</div>
-
+<div class="w-full h-20 bg-[#f4f4f4]"></div>
 
 <style>
     .custom-shape-divider-bottom-1706781815 {
